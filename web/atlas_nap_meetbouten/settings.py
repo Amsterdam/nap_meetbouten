2ea23f1519985b19d7edd7e364a896f82e1ce474
Richard Jansen <richard.jansen@lajos.nl>
2016-01-19 16:10:43 +0100

    project layout changes

--- web/atlas_nap_meetbouten/settings.py
+++ web/atlas_nap_meetbouten/settings.py
@@ -112,8 +112,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
 
-CRISPY_TEMPLATE_PACK = "bootstrap3"
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
