31f343d39601560be639126359bc1a35f6e3b48d
Yigal Duppen <yigal@publysher.nl>
2016-01-27 11:41:45 +0100

    Fix tests

--- web/atlas_nap_meetbouten/atlas_api/tests/test_datasets.py
+++ web/atlas_nap_meetbouten/atlas_api/tests/test_datasets.py
@@ -18,7 +18,7 @@ class BrowseDatasetsTestCase(APITestCase):
         PeilmerkFactory.create()
 
     def test_root(self):
-        response = self.client.get('/api/')
+        response = self.client.get('/metingen/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-Type'], 'application/hal+json')
 
@@ -27,7 +27,7 @@ class BrowseDatasetsTestCase(APITestCase):
 
     def test_lists(self):
         for url in self.datasets:
-            response = self.client.get('/api/{}/'.format(url))
+            response = self.client.get('/metingen/{}/'.format(url))
 
             self.assertEqual(response.status_code, 200, 'Wrong response code for {}'.format(url))
             self.assertEqual(response['Content-Type'], 'application/json', 'Wrong Content-Type for {}'.format(url))
@@ -37,7 +37,7 @@ class BrowseDatasetsTestCase(APITestCase):
 
     def test_details(self):
         for url in self.datasets:
-            response = self.client.get('/api/{}/'.format(url))
+            response = self.client.get('/metingen/{}/'.format(url))
 
             url = response.data['results'][0]['_links']['self']['href']
             detail = self.client.get(url)
