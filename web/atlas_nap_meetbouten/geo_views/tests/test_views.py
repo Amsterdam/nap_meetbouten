20c6aa3be4f7c7a15da8420b52b191f11abf7149
Richard Jansen <richard.jansen@lajos.nl>
2016-02-23 11:09:45 +0100

    added missing fields in geo_views

--- web/atlas_nap_meetbouten/geo_views/tests/test_views.py
+++ web/atlas_nap_meetbouten/geo_views/tests/test_views.py
@@ -19,15 +19,21 @@ class ViewsTest(TestCase):
         row = self.get_row('geo_nap_peilmerk')
         self.assertEqual(row['id'], l.id)
         self.assertIn("geometrie", row)
+        self.assertEqual(row['uri'],
+                         'http://update.me/nap/peilmerk/{}/'.format(l.id))
 
     def test_meetbouten_meetbout(self):
         mb = meetbouten_factories.MeetboutFactory.create()
         row = self.get_row('geo_meetbouten_meetbout')
         self.assertEqual(row['id'], mb.id)
         self.assertIn("geometrie", row)
+        self.assertEqual(row['uri'],
+                         'http://update.me/meetbouten/meetbout/{}/'.format(mb.id))
 
     def test_meetbouten_referentiepunt(self):
         rp = meetbouten_factories.ReferentiepuntFactory.create()
         row = self.get_row('geo_meetbouten_referentiepunt')
         self.assertEqual(row['id'], rp.id)
         self.assertIn("geometrie", row)
+        self.assertEqual(row['uri'],
+                         'http://update.me/meetbouten/referentiepunt/{}/'.format(rp.id))
