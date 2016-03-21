03db6fdfd381f0c7c8621c74accb1890b734cc47
Yigal Duppen <yigal@publysher.nl>
2016-03-21 16:10:36 +0100

    Use netcat to wait for open ports

--- web/atlas_nap_meetbouten/docker-entrypoint.sh
+++ web/atlas_nap_meetbouten/docker-entrypoint.sh
@@ -5,6 +5,20 @@ set -e
 
 cd /app
 
+# wait for elastic
+while ! nc -z ${ELASTIC_PORT_9200_TCP_ADDR} ${ELASTIC_PORT_9200_TCP_PORT}
+do
+	echo "Waiting for elastic..."
+	sleep 0.1
+done
+
+# wait for postgres
+while ! nc -z ${DATABASE_PORT_5432_TCP_ADDR} ${DATABASE_PORT_5432_TCP_PORT}
+do
+	echo "Waiting for postgres..."
+	sleep 0.1
+done
+
 # collect static files
 python manage.py collectstatic --noinput
 
