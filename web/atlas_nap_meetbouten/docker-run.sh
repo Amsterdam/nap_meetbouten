861ded4ee5af8065f68d8540130dbdacc242b779
Yigal Duppen <yigal@publysher.nl>
2016-09-14 14:24:55 +0200

    Differentiate between run and migrate

--- web/atlas_nap_meetbouten/docker-run.sh
+++ web/atlas_nap_meetbouten/docker-run.sh
@@ -3,15 +3,9 @@
 set -u   # crash on missing env variables
 set -e   # stop on any error
 
-cd /app
-
 source docker-wait.sh
 
-# migrate database tables
-yes yes | python manage.py migrate --noinput
-
-# create geotables and indices
-python manage.py create_geo_tables
+source docker-migrate.sh || echo "Could not migrate, ignoring"
 
 # run uwsgi
 exec uwsgi --ini /app/uwsgi.ini
