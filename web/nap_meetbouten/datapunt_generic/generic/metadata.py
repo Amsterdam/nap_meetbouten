9c42dc5e88a54412a5cf5a9af6c16b3b04df5387
Evert Lammerts <evert@lucipher.net>
2017-01-09 13:17:59 +0100

    removed unused code

--- web/nap_meetbouten/datapunt_generic/generic/metadata.py
+++ web/nap_meetbouten/datapunt_generic/generic/metadata.py
@@ -1,39 +1,24 @@
-# Python
 import datetime
 import requests
 import os
-# Project
-from datapunt_generic.generic import uva2
 
 METADATA_URL = os.getenv('METADATA_URL', '')
 
 
 class UpdateDatasetMixin(object):
     """
-    Mixin to update metadata API from import scripts.
+    Mixin to update metadata API from import scripts. (Copied from BAG)
 
     It will call the api-acc or api domain based on hostname.
 
     usage:
 
     - set dataset_id to correct dataset
-    - call self.update_metadata_uva2 for UVA2 files, or
-    - call self.update_metadata_onedate for CSV files, or
-    - call self.update_metadata_date for datasets without date in de filename
+    - call self.update_metadata_date
 
     These calls will return a Response object, or None when no date was passed.
     """
     dataset_id = None
-
-    def update_metadata_uva2(self, path, code):
-        filedate = uva2.get_uva2_filedate(path, code)
-
-        return self.update_metadata_date(filedate)
-
-    def update_metadata_onedate(self, path, code):
-        filedate = uva2.get_filedate(path, code)
-
-        return self.update_metadata_date(filedate)
 
     def update_metadata_date(self, date):
         if not date or not METADATA_URL:
