a2f9471ff4b6a5bce8ee3f818a957ed3e514a9c6
rbarneveld <r.van.barneveld@amsterdam.nl>
2017-01-10 14:28:31 +0100

    TG-539 upgrade to docker-compose 2.1

--- web/nap_meetbouten/docker-wait.sh
+++ web/nap_meetbouten/docker-wait.sh
@@ -7,12 +7,12 @@ set -e
 while ! nc -z elasticsearch 9200
 do
 	echo "Waiting for elastic..."
-	sleep 0.1
+	sleep 2
 done
 
 # wait for postgres
 while ! nc -z database 5432
 do
 	echo "Waiting for postgres..."
-	sleep 0.1
+	sleep 2
 done
