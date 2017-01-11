a5d54e959c5db29be8dbe57032139d161d1232dd
Thijs Creemers <thijs.creemers@gmail.com>
2017-01-11 13:05:37 +0100

    added wait to .jenkins/docker-compose remove the calls from scripts

--- web/nap_meetbouten/docker-run.sh
+++ web/nap_meetbouten/docker-run.sh
@@ -3,8 +3,6 @@
 set -u   # crash on missing env variables
 set -e   # stop on any error
 
-#source docker-migrate.sh || echo "Could not migrate, ignoring"
-
 # run uwsgi
 exec uwsgi --ini /app/uwsgi.ini
 
