32f05709bc76a14ba44fa82bb8185d8a86e99ed6
Stephan Zaat <s.zaat@amsterdam.nl>
2017-05-03 11:24:40 +0200

    Temp to test run import NAP with EL5

--- web/.jenkins-import/import.sh
+++ web/.jenkins-import/import.sh
@@ -9,12 +9,15 @@ dc() {
 	docker-compose -p nap -f ${DIR}/docker-compose.yml $*
 }
 
-trap 'dc kill ; dc rm -f' EXIT
-
-rm -rf ${DIR}/backups
-mkdir -p ${DIR}/backups
+#trap 'dc kill ; dc rm -f' EXIT
+#
+#rm -rf ${DIR}/backups
+#mkdir -p ${DIR}/backups
 
 dc build
-dc run --rm importer
-dc run --rm db-backup
-dc run --rm el-backup
+#dc run --rm importer
+#dc run --rm db-backup
+#dc run --rm el-backup
+dc run importer
+dc run db-backup
+dc run el-backup
