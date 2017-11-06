79c21ad98fdd3ae239edf50d88b03e6bb8535578
Stephan Preeker <stephan@preeker.net>
2017-11-06 12:06:29 +0100

    fix nap import

--- web/.jenkins-import/import.sh
+++ web/.jenkins-import/import.sh
@@ -9,12 +9,21 @@ dc() {
 	docker-compose -p nap -f ${DIR}/docker-compose.yml $*
 }
 
-trap 'dc kill ; dc rm -f' EXIT
+trap 'dc down ; dc rm -f' EXIT
 
 rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
-dc build
+dc up -d elasticsearch
+dc up -d database
+
+echo 'lets print some logs..'
+
+dc logs database
+dc logs elasticsearch
+
+dc logs elasticsearch
+
 dc run --rm importer
 dc run --rm db-backup
 dc run --rm el-backup
