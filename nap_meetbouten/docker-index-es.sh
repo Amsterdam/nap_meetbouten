2d82bafc4a23c5191d50af73c6eaf853ca243253
Stephan Zaat <s.zaat@amsterdam.nl>
2018-01-04 16:51:25 +0100

    BAS-556 Testing new style DB import

--- nap_meetbouten/docker-index-es.sh
+++ nap_meetbouten/docker-index-es.sh
@@ -6,26 +6,4 @@ set -x   # log every command.
 
 source docker-wait.sh
 
-# clear elasticindices
-python manage.py elastic_indices --delete
-
 python manage.py elastic_indices meetbouten --build
-
-FAIL=0
-
-for job in `jobs -p`
-do
-	echo $job
-	wait $job || let "FAIL+=1"
-done
-
-echo $FAIL
-
-if [ "$FAIL" == "0" ];
-then
-    echo "YAY!"
-else
-    echo "FAIL! ($FAIL)"
-    echo 'Elastic Import Error. 1 or more workers failed'
-    exit 1
-fi
