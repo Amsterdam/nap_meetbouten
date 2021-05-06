5c5751da0630937832edf89c91ea1567f18ef43f
jjmurre <jan.murre@gmail.com>
2021-05-06 10:07:30 +0200

    Apply black in separate commit.

--- nap_meetbouten/nap_meetbouten/settings/settings.py
+++ nap_meetbouten/nap_meetbouten/settings/settings.py
@@ -5,42 +5,41 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 from nap_meetbouten.settings.settings_common import BASE_DIR
-from nap_meetbouten.settings.settings_common import * # noqa F403
+from nap_meetbouten.settings.settings_common import *  # noqa F403
 
 
-from nap_meetbouten.settings.settings_databases import LocationKey,\
-    get_docker_host,\
-    get_database_key,\
-    OVERRIDE_HOST_ENV_VAR,\
-    OVERRIDE_PORT_ENV_VAR
+from nap_meetbouten.settings.settings_databases import (
+    LocationKey,
+    get_docker_host,
+    get_database_key,
+    OVERRIDE_HOST_ENV_VAR,
+    OVERRIDE_PORT_ENV_VAR,
+)
 
 from .checks import check_elasticsearch  # noqa
 from .checks import check_database  # noqa
 
 
-NO_INTEGRATION_TEST = os.getenv('NO_INTEGRATION_TEST', True)
+NO_INTEGRATION_TEST = os.getenv("NO_INTEGRATION_TEST", True)
 NO_INTEGATION_TEST = True
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-insecure_key = 'insecure'
-SECRET_KEY = os.getenv('SECRET_KEY', insecure_key)
+insecure_key = "insecure"
+SECRET_KEY = os.getenv("SECRET_KEY", insecure_key)
 
 DEBUG = SECRET_KEY == insecure_key
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
-PARTIAL_IMPORT = dict(
-    numerator=0,
-    denominator=1
-)
+PARTIAL_IMPORT = dict(numerator=0, denominator=1)
 
-STATIC_ROOT = 'static/'
-STATIC_URL = '/nap/static/'
+STATIC_ROOT = "static/"
+STATIC_URL = "/nap/static/"
 
-ROOT_URLCONF = 'nap_meetbouten.urls'
+ROOT_URLCONF = "nap_meetbouten.urls"
 
 
 # Database
@@ -48,38 +47,36 @@ ROOT_URLCONF = 'nap_meetbouten.urls'
 
 DATABASE_OPTIONS = {
     LocationKey.docker: {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.getenv('DATABASE_NAME', 'nap'),
-        'USER': os.getenv('DATABASE_USER', 'nap'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'insecure'),
-        'HOST': 'database',
-        'PORT': '5432'
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": os.getenv("DATABASE_NAME", "nap"),
+        "USER": os.getenv("DATABASE_USER", "nap"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "insecure"),
+        "HOST": "database",
+        "PORT": "5432",
     },
     LocationKey.local: {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.getenv('DATABASE_NAME', 'nap'),
-        'USER': os.getenv('DATABASE_USER', 'nap'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'insecure'),
-        'HOST': get_docker_host(),
-        'PORT': '5401'
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": os.getenv("DATABASE_NAME", "nap"),
+        "USER": os.getenv("DATABASE_USER", "nap"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "insecure"),
+        "HOST": get_docker_host(),
+        "PORT": "5401",
     },
     LocationKey.override: {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.getenv('DATABASE_NAME', 'nap'),
-        'USER': os.getenv('DATABASE_USER', 'nap'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'insecure'),
-        'HOST': os.getenv(OVERRIDE_HOST_ENV_VAR),
-        'PORT': os.getenv(OVERRIDE_PORT_ENV_VAR, '5432'),
-        'CONN_MAX_AGE': 30,
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": os.getenv("DATABASE_NAME", "nap"),
+        "USER": os.getenv("DATABASE_USER", "nap"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "insecure"),
+        "HOST": os.getenv(OVERRIDE_HOST_ENV_VAR),
+        "PORT": os.getenv(OVERRIDE_PORT_ENV_VAR, "5432"),
+        "CONN_MAX_AGE": 30,
     },
 }
 
-DATABASES = {
-    'default': DATABASE_OPTIONS[get_database_key()]
-}
+DATABASES = {"default": DATABASE_OPTIONS[get_database_key()]}
 
-EL_HOST_VAR = os.getenv('ELASTIC_HOST_OVERRIDE')
-EL_PORT_VAR = os.getenv('ELASTIC_PORT_OVERRIDE', '9200')
+EL_HOST_VAR = os.getenv("ELASTIC_HOST_OVERRIDE")
+EL_PORT_VAR = os.getenv("ELASTIC_PORT_OVERRIDE", "9200")
 
 ELASTIC_OPTIONS = {
     LocationKey.docker: ["http://elasticsearch:9200"],
@@ -90,44 +87,38 @@ ELASTIC_OPTIONS = {
 ELASTIC_SEARCH_HOSTS = ELASTIC_OPTIONS[get_database_key()]
 
 ELASTIC_INDICES = {
-    'MEETBOUTEN': 'meetbouten',
-    'NAP': 'nap',
+    "MEETBOUTEN": "meetbouten",
+    "NAP": "nap",
 }
 
-TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
+TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
 if TESTING:
     for k, v in ELASTIC_INDICES.items():
-        ELASTIC_INDICES[k] += 'test'
+        ELASTIC_INDICES[k] += "test"
 
-BATCH_SETTINGS = dict(
-    batch_size=500
-)
+BATCH_SETTINGS = dict(batch_size=500)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 REST_FRAMEWORK = dict(
     PAGE_SIZE=25,
     MAX_PAGINATE_BY=10000,
-
     UNAUTHENTICATED_USER={},
     UNAUTHENTICATED_TOKEN={},
-
     DEFAULT_PAGINATION_CLASS=("datapunt_api.pagination.HALPagination",),
     DEFAULT_RENDERER_CLASSES=(
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer'
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ),
-    DEFAULT_FILTER_BACKENDS=(
-        "django_filters.rest_framework.DjangoFilterBackend"
-    ),
+    DEFAULT_FILTER_BACKENDS=("django_filters.rest_framework.DjangoFilterBackend"),
 )
 
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
-X_FRAME_OPTIONS = 'DENY'
+X_FRAME_OPTIONS = "DENY"
 
-HEALTH_MODEL = 'nap.Peilmerk'
+HEALTH_MODEL = "nap.Peilmerk"
 
 NAP_DECIMAL_PLACES = 4
 NAP_MAX_DIGITS = 7  # if this is not enough, we have bigger problems
@@ -136,62 +127,56 @@ ZAKKING_DECIMAL_PLACES = 13
 ZAKKING_MAX_DIGITS = 20
 
 SWAGGER_SETTINGS = {
-    'exclude_namespaces': [],
-    'api_version': '0.1',
-    'api_path': '/',
-
-    'enabled_methods': [
-        'get',
+    "exclude_namespaces": [],
+    "api_version": "0.1",
+    "api_path": "/",
+    "enabled_methods": [
+        "get",
     ],
-
-    'api_key': '',
-    'USE_SESSION_AUTH': False,
-    'VALIDATOR_URL': None,
-
-    'is_authenticated': False,
-    'is_superuser': False,
-
-    'permission_denied_handler': None,
-    'resource_access_handler': None,
-
-    'protocol': 'https' if not DEBUG else '',
-    'base_path': 'acc.api.data.amsterdam.nl/meetbouten/docs' if DEBUG else '127.0.0.1:8000/meetbouten/docs',  # noqa
-
-    'info': {
-        'contact': 'atlas.basisinformatie@amsterdam.nl',
-        'description': 'This is the Meetbouten API server.',
-        'license': 'Mozilla Public License Version 2.0',
-        'licenseUrl': 'https://www.mozilla.org/en-US/MPL/2.0/',
-        'termsOfServiceUrl': 'https://data.amsterdam.nl/terms/',
-        'title': 'Meetbouten en NAP API',
+    "api_key": "",
+    "USE_SESSION_AUTH": False,
+    "VALIDATOR_URL": None,
+    "is_authenticated": False,
+    "is_superuser": False,
+    "permission_denied_handler": None,
+    "resource_access_handler": None,
+    "protocol": "https" if not DEBUG else "",
+    "base_path": "acc.api.data.amsterdam.nl/meetbouten/docs"
+    if DEBUG
+    else "127.0.0.1:8000/meetbouten/docs",  # noqa
+    "info": {
+        "contact": "atlas.basisinformatie@amsterdam.nl",
+        "description": "This is the Meetbouten API server.",
+        "license": "Mozilla Public License Version 2.0",
+        "licenseUrl": "https://www.mozilla.org/en-US/MPL/2.0/",
+        "termsOfServiceUrl": "https://data.amsterdam.nl/terms/",
+        "title": "Meetbouten en NAP API",
     },
-
-    'doc_expansion': 'list',
+    "doc_expansion": "list",
 }
 
-FILE_URL_DOMAIN = os.getenv('FILE_URL_DOMAIN', "https://files.data.amsterdam.nl")
-FILE_URL_PATH = os.getenv('FILE_URL_PATH', "/rollagen")
+FILE_URL_DOMAIN = os.getenv("FILE_URL_DOMAIN", "https://files.data.amsterdam.nl")
+FILE_URL_PATH = os.getenv("FILE_URL_PATH", "/rollagen")
 
 OBJECTSTORE = {
-    'auth_version': '2.0',
-    'authurl': 'https://identity.stack.cloudvps.com/v2.0',
-    'user': 'GOB_user',
-    'key': os.getenv('GOB_OBJECTSTORE_PASSWORD', 'insecure'),
-    'tenant_name': 'BGE000081_GOB',
-    'os_options': {
-        'tenant_id': '2ede4a78773e453db73f52500ef748e5',
-        'region_name': 'NL',
-    }
+    "auth_version": "2.0",
+    "authurl": "https://identity.stack.cloudvps.com/v2.0",
+    "user": "GOB_user",
+    "key": os.getenv("GOB_OBJECTSTORE_PASSWORD", "insecure"),
+    "tenant_name": "BGE000081_GOB",
+    "os_options": {
+        "tenant_id": "2ede4a78773e453db73f52500ef748e5",
+        "region_name": "NL",
+    },
 }
 
-PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', '..'))
+PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", ".."))
 
-DIVA_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'data'))
+DIVA_DIR = os.path.abspath(os.path.join(PROJECT_DIR, "data"))
 
 if not os.path.exists(DIVA_DIR):
-    DIVA_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'nap_meetbouten', 'diva'))
-    print("Geen lokale DIVA bestanden gevonden, maak gebruik van testset",
-          DIVA_DIR, "\n")
+    DIVA_DIR = os.path.abspath(os.path.join(PROJECT_DIR, "nap_meetbouten", "diva"))
+    print("Geen lokale DIVA bestanden gevonden, maak gebruik van testset", DIVA_DIR, "\n")
 
 
 # The following JWKS data was obtained in the authz project :  jwkgen -create -alg ES256
@@ -216,14 +201,10 @@ JWKS_TEST_KEY = """
     }
 """
 
-DATAPUNT_AUTHZ = {
-    'JWKS': os.getenv('PUB_JWKS', JWKS_TEST_KEY)
-}
+DATAPUNT_AUTHZ = {"JWKS": os.getenv("PUB_JWKS", JWKS_TEST_KEY)}
 
-SENTRY_DSN = os.getenv('SENTRY_DSN')
+SENTRY_DSN = os.getenv("SENTRY_DSN")
 if SENTRY_DSN:
     sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        environment="nap_meetbouten",
-        integrations=[DjangoIntegration()]
+        dsn=SENTRY_DSN, environment="nap_meetbouten", integrations=[DjangoIntegration()]
     )
